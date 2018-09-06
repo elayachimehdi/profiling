53dbd21fc3aa636f2df4b540ba2f6cd3cd20f6e7
elayachimehdi <elayachimehdi@gmail.com>
2018-09-06 19:31:22 +0100

    Clean Data

--- data_preparation.py
+++ data_preparation.py
@@ -1,4 +1,8 @@
 '''Avant de lancer le script il faut metter sa console sur le dossier profiling (master branch)'''
+
+#Recupérer la reponse ou le client defini son profil 
+
+
 #Importation des librairies
 import pyodbc as db
 import pandas as pd
@@ -9,24 +13,31 @@ import numpy as np
 con = db.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=ZBOOK;Trusted_Connection=yes;DATABASE=Peaqock')
 #cur = con.cursor()
 
+
+
 #Requete pour extraire les réponses au questionnaire (Table  KYC)
-query_kyc='SELECT * FROM Peaqock.dbo.KYC'
-df = pd.read_sql(query_kyc,con)
+df = pd.read_sql('SELECT * FROM Peaqock.dbo.KYC',con)
 #Requete pour extraire les questions du questionnaire (Table  Questionnaire)
-'''query_questionnaire='SELECT * FROM Peaqock.dbo.Questionnaire'
-questions = pd.read_sql(query_questionnaire,con)'''
-
-# Suppression des colonnes "vides"
-from fonctions import del_columns, del_rows
-df = del_columns(df)
-df = del_rows(df)
+questions = pd.read_sql('SELECT * FROM Peaqock.dbo.Questionnaire',con)
 
 
-#Remplacement des chaines de caractère
+
+# Suppression des colonnes "vides" ####################################################################################################################################
+from fonctions import del_columns, del_rows, index_to_question
+questions_to_delete= [28,34,36]+list(range(38,44))  #Les questions de la validation du questionnaire
+delete = index_to_question(questions_to_delete)
+df = df.drop(delete,axis = 1)
+df = del_columns(df)   #Les colonnes vides à 50%
+df = del_rows(df)      #Les lignes vides à 50%
+
+
+
+#Remplacement des chaines de caractère ####################################################################################################################################
 from changements import OLD, NEW
 df = df.replace(OLD,NEW)
 
-#Imputation des données
+
+#Imputation des données ####################################################################################################################################
 from sklearn.preprocessing import Imputer
 labels = df.columns
 imptr = Imputer(missing_values= 'NaN', strategy = 'mean', axis = 0)
@@ -38,11 +49,46 @@ df = np.around(df)
 df = pd.DataFrame(df, columns = labels)
 
 
-#Les variables catégoriques 
-from fonctions import categoriser
-from changements import Q36_old,Q45_old
-df = categoriser(dataframe = df, index = df.columns.get_loc('Q36'), labels = Q36_old)
-df = categoriser(dataframe = df, index = df.columns.get_loc('Q45'), labels = Q45_old)
+#Scoring des données ##########################################################################################################################################
+from fonctions import summer
+
+#Connaissance du marché (questions : 1,2,3,4,5,7,9)
+df[['Q2','Q3','Q4','Q5','Q7']] = df[['Q2','Q3','Q4','Q5','Q7']].multiply(5, axis = 1) 
+df = summer(df, ['Q2','Q3','Q4','Q5','Q7'], label = 'instruments_connus')
+df[['Q9']] = df[['Q9']].multiply(10, axis = 1) 
+df = summer(df,['Q1','instruments_connus','Q9'], label = 'connaissance_marche')
+
+#Tolerance au risque (questions : 10,11,12,13,14,19,20,29
+df[index_to_question([10,14])] = df[index_to_question([10,14])].multiply(15, axis = 1) 
+df[index_to_question([11,12,13])] = df[index_to_question([11,12,13])].multiply(5, axis = 1) 
+df[index_to_question([29])] = df[index_to_question([29])].multiply(15, axis = 1) 
+df = summer(df,index_to_question([10,11,12,13,14,19,20,29]), label = 'tolerance_risque')
+
+#Situation patrimoniale (questions : 45 )
+df[index_to_question([45])] = df[index_to_question([45])].multiply(15, axis = 1) 
+df['situation_patrimoniale'] = df[index_to_question([45])]
+df = df.drop(index_to_question([45]), axis=1)
+
+#Horizon (questions : 26 )
+df['horizon'] = df[index_to_question([26])]
+df = df.drop(index_to_question([26]), axis=1)
+
+#Objectif d'investissement(questions : 21,22,23,24,32 )
+df[index_to_question([21])] = df[index_to_question([21])].multiply(15, axis = 1) 
+df[index_to_question([22])] = df[index_to_question([22])].multiply(10, axis = 1) 
+df[index_to_question([23])] = df[index_to_question([23])].multiply(5, axis = 1) 
+df[index_to_question([24])] = df[index_to_question([24])].multiply(20, axis = 1) 
+df = summer(df,index_to_question([21,22,23,24,32]), label = 'objectif_investissement')
+
+#Temps disponible (question : 30)
+df['temps_disponible'] = df[index_to_question([30])]
+df = df.drop(index_to_question([30]), axis=1)
+
+#Mode de gestion (question : 16,18)
+table = np.array(df[['Q16','Q18']].values).T
+scalar = table[0]*table[1]
+df = df.drop(['Q16','Q18'], axis=1)
+df['mode_gestion']=pd.DataFrame(scalar)
 
 
 
@@ -53,39 +99,11 @@ df = categoriser(dataframe = df, index = df.columns.get_loc('Q45'), labels = Q45
 
 
 
-"""
-'''Avant de lancer le script il faut metter sa console sur le dossier profiling (master branch)'''
-#Importation des librairies
-import pyodbc as db
-import pandas as pd
 
 
-#Initialisation de la connection
-con = db.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=ZBOOK;Trusted_Connection=yes;DATABASE=Peaqock')
-#cur = con.cursor()
-
-#Requete pour extraire les réponses au questionnaire (Table  KYC)
-query_kyc='SELECT * FROM Peaqock.dbo.KYC'
-df = pd.read_sql(query_kyc,con)
-
-#Requete pour extraire les réponses au questionnaire (Table  KYC)
-query_questionnaire='SELECT * FROM Peaqock.dbo.Questionnaire'
-questions = pd.read_sql(query_questionnaire,con)
 
 
-# Nettoyage du Dataframe KYC
-from fonctions import index_to_question
-Nulls = pd.DataFrame(df.isnull().sum())
-seuil = len(df)/2
-to_delete = [i for i in range(len(Nulls)) if Nulls[0][i]>seuil]
-df = df.drop(index_to_question(to_delete), axis = 1)
 
-
-#Remplacment
-from changements import OLD, NEW
-df = df.replace(OLD,NEW)
-
-"""
 
 
 
--- fonctions.py
+++ fonctions.py
@@ -2,13 +2,25 @@
 import pandas as pd
 import numpy as np
 
+##########################################################################################################################################################
+
 def index_to_question(indexes):
-    #Takes a list of indexes and returns question labels
+    #Takes a list of indexes and returns question labels for the very first part 
     L = []
     for index in indexes:
-        L.append('Q'+str(index+1))
+        L.append('Q'+str(index))
     return L
 
+##########################################################################################################################################################
+    
+def label_to_index(df, labels):
+    indexes = []
+    for string in labels:
+        index = df.columns.get_loc(string)
+        indexes.append(index)
+    return indexes
+
+##########################################################################################################################################################
 
 def del_columns(DataFrame, rate = 0.5 ):
     #Takes a dataframe and a the rate of missing values allowed in each column
@@ -16,10 +28,11 @@ def del_columns(DataFrame, rate = 0.5 ):
     df = DataFrame
     empty = pd.DataFrame(df.isnull().sum())
     threshold = len(df)*rate
-    to_delete = [i for i in range(len(empty)) if empty[0][i]>threshold]
-    df = df.drop(index_to_question(to_delete), axis = 1) 
+    to_delete = [DataFrame.columns[i] for i in range(len(empty)) if empty[0][i]>threshold]
+    df = df.drop(to_delete, axis = 1) 
     return df
 
+##########################################################################################################################################################
 
 def del_rows(DataFrame, rate = 0.5 ):
     #Takes a dataframe and a the rate of missing values allowed in each row
@@ -31,19 +44,57 @@ def del_rows(DataFrame, rate = 0.5 ):
     df = df.drop(to_delete) 
     return df
 
+##########################################################################################################################################################
+
 def categoriser(dataframe = pd.DataFrame([]), index = None, s = pd.Series([]), labels=None):
     #takes s of type pd.Series which is a categorical numerised variable
+    #Or take datafram with index to catch pd.Series
     #returns a DataFrame of binary vectors of the categories
     if not s.empty : 
         data = pd.get_dummies(np.where(s==s.unique()[0], None, s))
         if labels != None :
-            print('columns :', data.columns)
             labels = [labels[int(i)] for i in data.columns]
             data.columns = labels
         return data
+    
     if not dataframe.empty :
         df = categoriser(s = dataframe.iloc[:,index], labels = labels)
         frames = [dataframe.iloc[:,:index], df, dataframe.iloc[:,index+1:]]
         return pd.concat(frames, axis = 1)
-    
+
+##########################################################################################################################################################    
+
+def summer(dataframe, columns, label = None):
+    #Takes dataframe, columns, label
+    if label == None : 
+        label = 'Somme des colonnes : '
+        for col in columns: 
+            label += str(col)
+        return summer(dataframe, columns, label )
+    else : 
+        
+        if not isinstance(columns[0],int):
+            return summer(dataframe, label_to_index(dataframe, columns), label)
+        else :
+            first_index = columns[0]
+            first_df = dataframe.iloc[:,:first_index]
+            summed_df = pd.DataFrame(dataframe.iloc[:,columns].sum(axis = 1))
+            summed_df.columns = [label]
+            to_delete = [dataframe.columns[index] for index in columns]
+            last_df = dataframe.iloc[:,first_index:].drop(to_delete, axis = 1)
+            df = pd.concat([first_df,summed_df,last_df], axis=1)
+            return df
+            
+##########################################################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
 
